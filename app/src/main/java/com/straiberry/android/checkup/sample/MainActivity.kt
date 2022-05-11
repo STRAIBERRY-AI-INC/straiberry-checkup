e0c2d5b6e38c44df10204e75f4573f38cac12ad3
shadman.adman <teamgashbeer@gmail.com>
2022-05-11 12:18:39 +0430

    Create sample project

--- app/src/main/java/com/straiberry/android/checkup/sample/MainActivity.kt
+++ app/src/main/java/com/straiberry/android/checkup/sample/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.straiberry.android.checkup.sample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.straiberry.android.checkup.common.helper.StraiberryCheckupSdkInfo
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        StraiberryCheckupSdkInfo.setTokenInfo(
+            getString(R.string.checkup_sdk_key),
+            "com.straiberry.android.checkup.sample"
+        )
+
+        StraiberryCheckupSdkInfo.setUniqueId(getString(R.string.unique_id))
+
+        StraiberryCheckupSdkInfo.setDisplayName(getString(R.string.display_name))
+
     }
 }
